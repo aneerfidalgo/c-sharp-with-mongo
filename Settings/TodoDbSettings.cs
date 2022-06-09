ca9d7e5c7d08ae518ae08ec6fae4032e034223fc
Aneer Fidalgo <aneer@Aneers-MacBook-Air.local>
2022-06-09 09:53:11 -0400

    c-sharp with mongo

---
+++ Settings/TodoDbSettings.cs
@@ -0,0 +1,10 @@
+namespace Settings;
+
+public class TodoDbSettings
+{
+    public string? ConnectionString { get; set; }
+
+    public string? DatabaseName { get; set; }
+
+    public string? CollectionName { get; set; }
+}
